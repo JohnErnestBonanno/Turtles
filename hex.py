5e4df11907a59f669529abc2a2f26ba38f549786
John Bonanno <john.ernest.bonanno@gmail.com>
2018-05-01 23:40:16 -0400

    Define HexUp/Down & Main Function

--- hex.py
+++ hex.py
@@ -3,7 +3,7 @@ import turtle
 my_turtle = turtle.Turtle()
 
 #set speed
-my_turtle.speed(2)
+
 
 #clear screen, move turtle back to initial position
 #my_turtle.reset()
@@ -19,11 +19,14 @@ def lhex():
     return()
 
 def slideup():
+    my_turtle.pu()
     my_turtle.lt(120)
     my_turtle.fd(distance)
     my_turtle.rt(60)
     my_turtle.fd(distance)
     my_turtle.rt(60)
+    my_turtle.pd()
+
 
 def rhex():
     for i in range(0,5):
@@ -34,16 +37,44 @@ def rhex():
     return()
 
 def slidedown():
+    my_turtle.pu()
     for i in range(0,4):
         my_turtle.fd(distance)
         my_turtle.rt(60)
     my_turtle.rt(120)
+    my_turtle.pd()
 
 def shiftl2r():
+    my_turtle.pu()
     my_turtle.fd(distance)
     my_turtle.lt(60)
     my_turtle.fd(distance)
     my_turtle.rt(60)
+    my_turtle.pd()
+
+def hexdown():
+    for i in range(0,5):
+        rhex()
+        slidedown()
+
+def hexup():
+    for i in range(0,5):
+        lhex()
+        slideup()
+
+def main():
+    speed = 5
+    my_turtle.speed(speed)
+    for i in range(0,5):
+        hexdown()
+        shiftl2r()
+        hexup()
+        shiftl2r()
+        speed += 5
+#need to add main funtion that inputs for either hexup() or hexdown() to go
+#one more
+
+
 
  #using slide down, can a line of hexagons going down
 
