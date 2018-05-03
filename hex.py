4c4a84420846b29a2c2382910c643ba1b69f27d2
John Bonanno <john.ernest.bonanno@gmail.com>
2018-05-02 23:34:33 -0400

    Create Main() Function

--- hex.py
+++ hex.py
@@ -2,34 +2,7 @@
 import turtle
 my_turtle = turtle.Turtle()
 
-#set speed
-
-
-#clear screen, move turtle back to initial position
-#my_turtle.reset()
-
-#distance = 25
-#vertdist = 2
-#hordist = 2
-
-def lhex(distance):
-    for i in range(0,5):
-        my_turtle.fd(distance)
-        my_turtle.lt(60)
-    my_turtle.fd(distance)
-    my_turtle.lt(60)
-    return()
-
-def slideup(distance):
-    my_turtle.pu()
-    my_turtle.lt(120)
-    my_turtle.fd(distance)
-    my_turtle.rt(60)
-    my_turtle.fd(distance)
-    my_turtle.rt(60)
-    my_turtle.pd()
-
-
+##Start Code for Hexdown()##
 def rhex(distance):
     for i in range(0,5):
         my_turtle.fd(distance)
@@ -40,11 +13,41 @@ def rhex(distance):
 
 def slidedown(distance):
     my_turtle.pu()
-    for i in range(0,4):
-        my_turtle.fd(distance)
-        my_turtle.rt(60)
     my_turtle.rt(120)
+    my_turtle.fd(distance)
+    my_turtle.lt(60)
+    my_turtle.fd(distance)
+    my_turtle.lt(60)
     my_turtle.pd()
+
+def hexdown(distance,vertdist):
+    for i in range(0,vertdist):
+        rhex(distance)
+        slidedown(distance)
+##End Code for Hexdown()##
+
+##Start Code for Hexup()###
+def lhex(distance):
+    for i in range(0,5):
+        my_turtle.fd(distance)
+        my_turtle.lt(60)
+    my_turtle.fd(distance)
+    my_turtle.lt(60)
+
+def slideup(distance):
+    my_turtle.pu()
+    my_turtle.lt(120)
+    my_turtle.fd(distance)
+    my_turtle.rt(60)
+    my_turtle.fd(distance)
+    my_turtle.rt(60)
+    my_turtle.pd()
+
+def hexup(distance,vertdist):
+    for i in range(0,vertdist):
+        lhex(distance)
+        slideup(distance)
+##End Code for Hexup()###
 
 def shiftl2r(distance):
     my_turtle.pu()
@@ -54,41 +57,38 @@ def shiftl2r(distance):
     my_turtle.rt(60)
     my_turtle.pd()
 
-def hexdown():
-    for i in range(0,vertdist):
-        rhex()
-        slidedown()
-
-def hexup():
-    for i in range(0,vertdist+1):
-        lhex()
-        slideup()
-
 def main(distance, vertdist, hordist):
-    speed = 5
+    speed = 10
     my_turtle.speed(speed)
-    hexdown()
-    shiftl2r(distance)
-    hexup()
-    shiftl2r(distance)
-    #speed += 5
+
+    for i in range(0,hordist):
+        hexdown(distance,vertdist+1)
+        shiftl2r(distance)
+        hexup(distance,vertdist)
+        shiftl2r(distance)
+        speed += 5
+
+
+
+
 #need to add main funtion that inputs for either hexup() or hexdown() to go
 #one more
 
 #make simple case function saying this is your number, and then another main function calling it.  Need
 #to figure out what can get refewrenced where.  Or need to simplfy the above to make sure I get hexdown/hexup
-#working how I want it to 
+#working how I want it to
 
 
  #using slide down, can a line of hexagons going down
 
 """
+
 if turn 120 degrees same direction every time
 turtle follows same path around 3 hexagons
 
 if turn 120 degrees, alternating directions
 turtuel follows same path around 2 hexagons
-
+"""
 """
 #try alternating left and right turns at end of function
 
@@ -99,3 +99,4 @@ def starhex():
     my_turtle.fd(distance)
     #call 6 times for starhex
     return()
+    """
